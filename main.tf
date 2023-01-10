2fbcb4b6da28b4f067c0ea31378b63544cb95312
Nguyen Tuan Anh <anh93.nguyentuan@gmail.com>
2023-01-10 23:28:31 +0700

    fix: Rename S3 bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+data "http" "infra" {
+  url = "https://raw.githubusercontent.com/TravelOffice/infrastructure-constant/master/${var.ENV}-infrastructure.json"
+}
+
 variable "LAMBDA_LAYERS" {
   description = "List params of lambda layers"
 }
@@ -7,9 +11,6 @@ variable "ENV" {
 variable "FEATURE_NAME" {
   description = "Feature name"
 }
-variable "S3_BUCKET" {
-  default = "codepipeline-artifacts20220108172710412300000001"
-}
 variable "TAGS" {
   description = "List tags"
 }
@@ -17,6 +18,10 @@ locals {
   root_path = format("%s/../../..", path.root)
 }
 
+locals {
+  constants = jsondecode(data.http.infra.body)
+  s3_bucket = local.constants["CODEPIPELINE_BUCKET"]
+}
 data "external" "layer_zip" {
   for_each = var.LAMBDA_LAYERS
   program = [
@@ -32,7 +37,7 @@ data "external" "layer_zip" {
 ## TODO: Temporary disable cause by codebuild upload error
 resource "aws_s3_object" "layer_object" {
   for_each    = var.LAMBDA_LAYERS
-  bucket      = var.S3_BUCKET
+  bucket      = local.s3_bucket
   key         = "execution/${var.ENV}-${var.FEATURE_NAME}-${each.key}-layer"
   source      = data.external.layer_zip[each.key].result.path
   source_hash = data.external.layer_zip[each.key].result.hash
@@ -44,7 +49,7 @@ resource "aws_lambda_layer_version" "layer" {
   # filename   = data.external.layer_zip[each.key].result.path
 
   ## TODO: Temporary disable cause by codebuild upload error
-  s3_bucket        = var.S3_BUCKET
+  s3_bucket        = local.s3_bucket
   s3_key           = aws_s3_object.layer_object[each.key].id
   source_code_hash = data.external.layer_zip[each.key].result.hash
 
